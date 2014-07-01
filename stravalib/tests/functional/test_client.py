884814a397939a26e70d4a2f6ba99097b77a40e6
gp <ghisprince@gmail.com>
2014-07-01 00:00:00 -0700

    test for get_segment_efforts

--- stravalib/tests/functional/test_client.py
+++ stravalib/tests/functional/test_client.py
@@ -125,8 +125,27 @@ class ClientTest(FunctionalTestBase):
         # Fetch leaderboard
         lb = segment.leaderboard
         self.assertEquals(15, len(lb)) # 10 top results, 5 bottom results
-        
-    
+
+    def test_get_segment_efforts(self):
+        efforts = self.client.get_segment_efforts(4357415,
+                                     start_date_local = "2012-12-23T00:00:00Z",
+                                     end_date_local   = "2012-12-23T11:00:00Z",)
+        print efforts
+
+        i = 0
+        for effort in efforts:
+            print effort
+            self.assertEqual('Jedi climb', effort.name)
+            self.assertEqual(4357415, effort.segment.id)
+            self.assertIsInstance(effort, model.BaseEffort)
+            effort_date = effort.start_date_local
+            self.assertEqual(effort_date.strftime("%Y-%m-%d"), "2012-12-23")
+            i+=1
+        print i
+
+        self.assertGreater(i, 2)
+
+
     def test_segment_explorer(self):
         bounds = (37.821362,-122.505373,37.842038,-122.465977)
         results = self.client.explore_segments(bounds)
