7d40040336458e66ab46541d93c1f9335322a426
Hans Lellelid <hans@xmpl.org>
2015-01-15 21:15:33 -0500

    Bumping version for development

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-version = '0.4.0'
+version = '0.4.1dev'
 
 news = os.path.join(os.path.dirname(__file__), 'docs', 'news.rst')
 news = open(news).read()
