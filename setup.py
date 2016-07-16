a894a992c875cc2b45560a29939966e1dc350dff
Hans Lellelid <hans@xmpl.org>
2016-07-16 14:39:56 -0400

    Bumping minor version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-version = '0.5.1'
+version = '0.6.0'
 
 news = os.path.join(os.path.dirname(__file__), 'docs', 'news.rst')
 news = open(news).read()
