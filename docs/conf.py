a5337b506ceed608fb7cd3c079d646d1663708ec
Leah Wasser <leah.wasser@colorado.edu>
2021-09-21 16:24:14 -0600

    workflow

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
