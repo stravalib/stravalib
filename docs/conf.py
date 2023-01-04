3199c7c9825c3353089e2da3abfd4f46fd872689
Leah Wasser <leah@pyopensci.org>
2023-01-03 19:15:47 -0700

    Add: documentation about precommit to docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,29 +14,33 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../stravalib'))
+
+sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../stravalib"))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
 # -- Project information -----------------------------------------------------
 
 import datetime
-import stravalib 
+
+import stravalib
 
 # General project info
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
-project = 'stravalib'
+project = "stravalib"
 
 # Automagically create the year vs hand code
-copyright = f"{datetime.date.today().year}, The {project} Developers"  # noqa: A001
+copyright = (
+    f"{datetime.date.today().year}, The {project} Developers"  # noqa: A001
+)
 # Grab the package version from the version attr
 if len(stravalib.__version__.split(".")) > 3:
     version = "dev"
@@ -48,12 +52,12 @@ else:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx_copybutton',
-    'sphinx.ext.autosummary',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+    "sphinx.ext.autosummary",
     "myst_nb",
     "sphinx_design",
     "sphinxext.opengraph",
@@ -64,33 +68,33 @@ extensions = [
 myst_enable_extensions = ["colon_fence"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = [
-    '_build',
-    'stravalib/tests',
-    'stravalib/tests/functional',
-    'stravalib/tests/unit',
-    'stravalib/tests/resources'
+    "_build",
+    "stravalib/tests",
+    "stravalib/tests/functional",
+    "stravalib/tests/unit",
+    "stravalib/tests/resources",
 ]
 
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 
 # The title in the left hand corner of the docs
 html_title = "Stravalib Docs"
 # Theme and css
 html_theme = "furo"
-# Add edit button to furo theme 
+# Add edit button to furo theme
 html_theme_options = {
     "source_repository": "https://github.com/stravalib/stravalib/",
     "source_branch": "master",
@@ -103,51 +107,53 @@ html_css_files = ["stravalib.css"]
 # Short title for the navigation bar.
 html_short_title = "Stravalib Python Package Documentation"
 
+# Instagram always throws 429 so ignore it
+linkcheck_ignore = [r"https://www.instagram.com/accounts/login/"]
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+# html_logo = None
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+# html_favicon = None
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = []
+# html_extra_path = []
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+# html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = True
+# html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+# html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#html_additional_pages = {}
+# html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+# html_domain_indices = True
 
 # If false, no index is generated.
-#html_use_index = True
+# html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
-#html_split_index = False
+# html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+# html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = True
@@ -158,25 +164,24 @@ html_show_copyright = True
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#html_use_opensearch = ''
+# html_use_opensearch = ''
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
+# html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'stravalibdoc'
+htmlhelp_basename = "stravalibdoc"
 
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'stravalib', u'stravalib Documentation',
-     [u'Hans Lellelid'], 1)
+    ("index", "stravalib", "stravalib Documentation", ["Hans Lellelid"], 1)
 ]
 
 # If true, show URL addresses after external links.
-#man_show_urls = False
+# man_show_urls = False
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -185,25 +190,31 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'stravalib', u'stravalib Documentation',
-   u'Hans Lellelid', 'stravalib', 'One line description of project.',
-   'Miscellaneous'),
+    (
+        "index",
+        "stravalib",
+        "stravalib Documentation",
+        "Hans Lellelid",
+        "stravalib",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
 
 # Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
+# texinfo_appendices = []
 
 # If false, no module index is generated.
-#texinfo_domain_indices = True
+# texinfo_domain_indices = True
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
+# texinfo_show_urls = 'footnote'
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
-#texinfo_no_detailmenu = False
+# texinfo_no_detailmenu = False
 
 # Example configuration for intersphinx: refer to the Python standard library.
 # intersphinx_mapping = {'http://docs.python.org/': None}
 
 # Default to using the order defined in source.
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
