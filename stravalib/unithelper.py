5f2f51550a51d3f72968d783d089bc0bbbae53ca
Jonatan Samoocha <jsamoocha@yahoo.com>
2023-01-27 10:15:46 +0100

    Add support for lazy loading related entities (#322)
    * Adds first working example of bound client for club members
    
    * Adds field converter for timezones
    
    * Replaces legacy Activity class by extension of generated model
    
    Adds backward-compatibility support for lazy loading using
    bound client.
    
    Refactors validators for nested entities to prevent many code
    repetitions.
    
    * Adds tests for endpoints related to added classes
    
    * Adds backward compatibility for is_authenticated_athlete()
    
    Moves existing test from functional to integration package.
    
    * Removes client methods for non-existing endpoints
    
    * Adds stats bound entity to athlete
    
    * Replaces legacy SegmentExplorerResult by extended version
    
    Adds nested entities for segments
    
    * Segment leaderboards are removed from the Strava API
    
    The endpoint url in the client returns 403.
    
    * Makes sure the type extension validators are run before the superclass ones
    
    If not, the values being processed here are the superclass (nested)
    objects, stripped of any potential undocumented attributes we may
    want to process.
    
    * Adds example processing of undocumented types
    
    * Adds minor clarification
    
    * Makes it easier to test against plain Pint instances
    
    * Makes sure bound client is added to BoundClientEntity instances
    
    * Improves BackwardCompatibilityMixin
    
    - Adds passing of bound_client to nested objects (or collections thereof)
    - Simplifies stuff by removing distinction between field conversions and
    unit conversions
    - Adds unit tests for type extension validators and attribute lookup
    
    * Makes sure client will be attached to segment efforts

--- stravalib/unithelper.py
+++ stravalib/unithelper.py
@@ -42,7 +42,10 @@ class Quantity(Q_):
 
     def __eq__(self, other):
         # sloppy, but temporary (because deprecated)
-        return self.q == other.q
+        try:
+            return self.q == other.q
+        except AttributeError:
+            return self.q == other  # maybe other is a plain Pint Quantity?
 
     def __int__(self):
         return int(self.magnitude)
