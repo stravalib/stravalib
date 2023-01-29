9d647b248e18016bd31312a5c99a88f75147ac47
Leah Wasser <leah@pyopensci.org>
2023-01-29 14:27:14 -0700

    Add: autodoc_pydantic to sphinx setup

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,7 @@ else:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinxcontrib.autodoc_pydantic",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.ifconfig",
@@ -63,6 +64,11 @@ extensions = [
     "sphinxext.opengraph",
     "sphinx_inline_tabs",
 ]
+
+# https://autodoc-pydantic.readthedocs.io/en/stable/users/installation.html
+autodoc_pydantic_model_show_json = True
+autodoc_pydantic_settings_show_json = False
+autosummary_generate = True
 
 # Colon fence for card support in md
 myst_enable_extensions = ["colon_fence"]
