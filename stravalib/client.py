46c538401a102b7976a85ffa5664d8895ec7293e
Émile Nadeau <nadeau.emile@gmail.com>
2023-07-29 20:50:51 +0000

    use after_epoch and before_epoch in param dict

--- stravalib/client.py
+++ stravalib/client.py
@@ -296,7 +296,7 @@ class Client:
 
         before_epoch = self._utc_datetime_to_epoch(before) if before else None
         after_epoch = self._utc_datetime_to_epoch(after) if after else None
-        params = dict(before=before, after=after)
+        params = dict(before=before_epoch, after=after_epoch)
         result_fetcher = functools.partial(
             self.protocol.get, "/athlete/activities", **params
         )
