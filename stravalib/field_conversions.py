a0d243d01e6bae89f0716353e638019059bd8657
Émile Nadeau <nadeau.emile@gmail.com>
2023-08-18 21:59:31 +0000

    type annotation for field conversion file

--- stravalib/field_conversions.py
+++ stravalib/field_conversions.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 import logging
 from collections.abc import Sequence
 from datetime import timedelta
 from functools import wraps
-from typing import Any, Callable, Optional, Union
+from typing import Callable, TypeVar
 
 import pytz
 from pytz.exceptions import UnknownTimeZoneError
@@ -11,10 +13,21 @@ from stravalib.strava_model import ActivityType, SportType
 
 LOGGER = logging.getLogger(__name__)
 
+T = TypeVar("T")
+U = TypeVar("U")
 
-def optional_input(field_conversion_fn: Callable) -> Callable:
+
+def optional_input(
+    field_conversion_fn: Callable[[T], U]
+) -> Callable[[T | None], U | None]:
+    """
+    Transform a one argument function to make the input optional.
+
+    When None is passed as argument the wrapped function return None.
+    """
+
     @wraps(field_conversion_fn)
-    def fn_wrapper(field_value: Any):
+    def fn_wrapper(field_value: T | None) -> U | None:
         if field_value is not None:
             return field_conversion_fn(field_value)
         else:
@@ -24,18 +37,18 @@ def optional_input(field_conversion_fn: Callable) -> Callable:
 
 
 @optional_input
-def enum_value(v: Union[ActivityType, SportType]) -> str:
+def enum_value(v: ActivityType | SportType) -> str:
     try:
         return v.__root__
     except AttributeError:
         LOGGER.warning(
             f"{v} is not an enum, returning itself instead of its value"
         )
-        return v
+        return v  # type: ignore[return-value]
 
 
 @optional_input
-def enum_values(enums: Sequence[Union[ActivityType, SportType]]) -> list:
+def enum_values(enums: Sequence[ActivityType | SportType]) -> list[str | None]:
     # Pydantic (1.x) has config for using enum values, but unfortunately
     # it doesn't work for lists of enums.
     # See https://github.com/pydantic/pydantic/issues/5005
@@ -51,7 +64,9 @@ def time_interval(seconds: int) -> timedelta:
 
 
 @optional_input
-def timezone(tz: str) -> Optional[pytz.timezone]:
+def timezone(
+    tz: str,
+) -> pytz._UTCclass | pytz.tzinfo.StaticTzInfo | pytz.tzinfo.DstTzInfo | None:
     if " " in tz:
         # (GMT-08:00) America/Los_Angeles
         tzname = tz.split(" ", 1)[1]
@@ -59,10 +74,9 @@ def timezone(tz: str) -> Optional[pytz.timezone]:
         # America/Los_Angeles
         tzname = tz
     try:
-        tz = pytz.timezone(tzname)
+        return pytz.timezone(tzname)
     except UnknownTimeZoneError as e:
         LOGGER.warning(
             f"Encountered unknown time zone {tzname}, returning None"
         )
-        tz = None
-    return tz
+        return None
