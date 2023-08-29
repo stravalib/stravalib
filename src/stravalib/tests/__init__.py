320634ef88312ef7f24933b09507e7886642211d
Leah Wasser <leah@pyopensci.org>
2023-08-29 16:09:36 -0400

    Fix: update changelog and noxfile
    Add src/ dir to all test calls
    
    Fix: update workflows to reflect new layout

--- src/stravalib/tests/__init__.py
+++ src/stravalib/tests/__init__.py
@@ -1,14 +1,7 @@
 import os.path
-import sys
 
-if sys.version_info < (2, 7):
-    # TODO: We can probably add this smartly to setup.py
-    try:
-        from unittest2 import TestCase
-    except ImportError:
-        raise Exception("Need unittest2 for running tests under python 2.6")
-else:
-    from unittest import TestCase
+# Removing unittest - exception for python  < 3.0 as we no longer support it
+from unittest import TestCase
 
 TESTS_DIR = os.path.dirname(__file__)
 RESOURCES_DIR = os.path.join(TESTS_DIR, "resources")
