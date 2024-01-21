c2561b6771fa88c5f9add1cf1b9807c4b4da3171
Sung Oh <109045159+soh55@users.noreply.github.com>
2024-01-21 13:16:23 -0600

    Use Pydantic V2 with V1 compatibility mode (#456)
    * Add Pydantic V1 adapters to use this library with later Pydantic versions
    
    * Update pyproject.toml
    
    * Update pydantic version
    
    * Add Pydantic V1 adapters to use this library with later Pydantic versions
    
    * Update changelog.md

--- src/stravalib/strava_model.py
+++ src/stravalib/strava_model.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 from datetime import datetime
 from typing import Literal, Optional
 
-from pydantic import BaseModel, Field
+from pydantic.v1 import BaseModel, Field
 
 
 class ActivityTotal(BaseModel):
