274f9912d726d42c3c6c76b81e72ba3fc87ececa
Jonatan Samoocha <jsamoocha@yahoo.com>
2024-06-23 09:50:11 +0200

    FIX: restores coverage of unit converter

--- src/stravalib/tests/unit/test_unithelper.py
+++ src/stravalib/tests/unit/test_unithelper.py
@@ -1,4 +1,22 @@
+import pytest
+
 from stravalib import unithelper as uh
+from stravalib.unithelper import _Quantity
+
+
+class DistanceQuantity(_Quantity):
+    unit = "feet"
+
+
+@pytest.mark.parametrize(
+    "expression,expected_unitless_result",
+    ((uh.ureg("feet") * 6, 1.83), (DistanceQuantity(6), 1.83), (2.0, 2.0)),
+)
+def test_unit_converter(expression, expected_unitless_result):
+    converter = uh.UnitConverter("meters")
+    assert converter(expression).magnitude == pytest.approx(
+        expected_unitless_result, abs=0.01
+    )
 
 
 def test_arithmetic_comparison_support():
